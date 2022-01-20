a2a13aef5ebfebbdabfa4eec152381c6d07bf9cb
wormjoo <wormjoo@naver.com>
2022-01-20 10:46:00 +0900

    Lesson 5

--- CryptoZombies/zombiefactory.sol
+++ CryptoZombies/zombiefactory.sol
@@ -1,6 +1,11 @@
 pragma solidity ^0.4.19;
 
-contract ZombieFactory {
+import "./ownable.sol";
+import "./safemath.sol";
+
+contract ZombieFactory is Ownable {
+
+    using SafeMath for uint256;
 
     event NewZombie(uint zombieId, string name, uint dna);
 
